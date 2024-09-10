9e37a4a6c86f7a3bf8084b40a1cd9abf68bad428
Thirumalai-tech-Developer <thirutechdeveloper@gmail.com>
2024-09-10 14:41:55 +0530

    navbar implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,28 @@
-const App = () => {
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contant';
 
+function App() {
   return (
-    <>
+    <Router>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Simple React App with Routing</h1>
+        <nav>
+          <Link to="/">Home</Link> | 
+          <Link to="/about">About</Link> | 
+          <Link to="/contact">Contact</Link>
+        </nav>
 
-    </>
-  )
+        {/* Define routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
